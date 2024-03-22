45309a9914dc1aec54a63a222817fa242d681b95
Rachyita <rachyitamehta@gmail.com>
2024-03-22 18:57:29 +0530

    created REST API's using Node & Express.js, used POSTMAN for API's, learnt to use middleware.

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
 const express = require("express");
-const users = require('./MOCK_DATA.json');
+const fs = require("fs");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
 
+// Middlwware - Plugin
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  fs.appendFile(
+    "log.txt",
+    `${Date.now()}: ${req.ip}: ${req.method}: ${req.path}\n`,
+    (err, data) => {
+      next();
+    }
+  );
+});
+
 //Routes
-app.get('/api/users',(req, res) => {
-  return res.json(users)
-})
+
+app.get("/users", (req, res) => {
+  const html = `
+  <ul>
+    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+  </ul>
+  `;
+  res.send(html);
+});
+
+// REST API
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.filter((user) => user.id !== id);
+    const deldata = users.find((user) => user.id === id);
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(user), (req, data) => {
+      return res.json({ status: "success", deldata });
+    });
+  });
+
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (req, data) => {
+    return res.json({ status: "success", id: users.length });
+  });
+});
+
 app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
