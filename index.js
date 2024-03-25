2b7a6ebdf1d0727d6a1965efec199bebada8f242
Rachyita <rachyitamehta@gmail.com>
2024-03-25 16:04:17 +0530

    worked on middleware, headers & status codes

--- index.js
+++ index.js
@@ -31,6 +31,9 @@ app.get("/users", (req, res) => {
 
 // REST API
 app.get("/api/users", (req, res) => {
+  res.setHeader("X-myName", "Rachyita"); // Custom header
+  // Always add X to custom headers
+  console.log(req.headers);
   return res.json(users);
 });
 
@@ -39,6 +42,7 @@ app
   .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if( !user) return res.status(404).json({ error: 'user not found'})
     return res.json(user);
   })
   .patch((req, res) => {
@@ -55,9 +59,19 @@ app
 
 app.post("/api/users", (req, res) => {
   const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({ msg: "All fields are required"});
+  }
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (req, data) => {
-    return res.json({ status: "success", id: users.length });
+    return res.status(201).json({ status: "success", id: users.length });
   });
 });
 
